dbbcda49dfb51acfc7421bedfc81c81d700aff92
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-11-24 21:45:17 -0700

    Not sure why http isn't working in python

--- program/program.py
+++ program/program.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 # Hello World
 from keys import *
-print(GetObaKey())
+import httplib
+conn = httplib.HTTPConnection('api.pugetsound.org')
+conn.request("HEAD", '/api/where/arrivals-and-departures-for-stop/1_52800.xml?key=' + GetObaKey())
+
+res = conn.getresponse()
+print res.status, res.reason
